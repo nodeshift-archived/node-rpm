9ee2c7bbd761cc1d7ea6eb42edd1f05712236197
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-07-07 07:04:31 +0200

    src: fix old_build_dir for build-base-image.sh
    This was updated in run.sh be missed for this file.

--- src/build-base-image.sh
+++ src/build-base-image.sh
@@ -3,7 +3,7 @@
 version=$(rpm -q --specfile --qf='%{version}\n' nodejs.spec | head -n1)
 echo "Building with version $version"
 
-old_build_dir=$(ls -d /root/rpmbuild_usr_src_debug/BUILD/node-v*)
+old_build_dir=/root/rpmbuild_usr_src_debug/BUILD/nodejs
 new_build_dir=$(dirname $old_build_dir)/node-v${version}
 echo "old_build_dir:" ${old_build_dir}
 echo "new_build_dir:" ${new_build_dir}
