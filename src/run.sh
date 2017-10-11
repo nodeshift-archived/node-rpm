b7cde5824a355368825806198392303a9444f6d8
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-10-11 10:54:22 +0200

    src: remove deps/openssl from sources

--- src/run.sh
+++ src/run.sh
@@ -4,7 +4,10 @@ version=$(rpm -q --specfile --qf='%{version}\n' nodejs.spec | head -n1)
 echo "Building version $version"
 
 curl -O http://nodejs.org/dist/v${version}/node-v${version}.tar.gz
-mv node-v${version}.tar.gz /root/rpmbuild/SOURCES
+tar -zxf node-v${version}.tar.gz
+rm -rf node-v${version}/deps/openssl
+tar -zcf node-v${version}-stripped.tar.gz node-v${version}
+mv node-v${version}-stripped.tar.gz /root/rpmbuild/SOURCES/node-v${version}.tar.gz
 
 ## Build the rpm
 rpmbuild -ba --noclean --define='basebuild 0' /usr/src/node-rpm/nodejs.spec
