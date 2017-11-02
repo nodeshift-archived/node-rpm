da6bf8e3663255eff7c80bf25972cab5007525ed
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-11-02 14:47:59 +0100

    build: switch to use CentOS instead of RHEL

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ echo "Building version $version"
 
 git clone https://github.com/bucharest-gold/node.git -b v${version}-rh node-v${version}
 tar -zcf node-v${version}-rh.tar.gz node-v${version}
-mv node-v${version}-rh.tar.gz /root/rpmbuild/SOURCES/node-v${version}-rh.tar.gz
+mv node-v${version}-rh.tar.gz /opt/app-root/src/rpmbuild/SOURCES/node-v${version}-rh.tar.gz
 
 ## Build the rpm
 rpmbuild -ba --noclean --define='basebuild 0' /usr/src/node-rpm/nodejs.spec
