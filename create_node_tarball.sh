16d626ca55b4147e1cd89f7183e2e5cff33591d8
Daniel Bevenius <daniel.bevenius@gmail.com>
2018-10-30 08:30:49 +0100

    src: update to use nodeshift org
    This commit updates the documents and code base to use the new nodeshift
    organisation.

--- create_node_tarball.sh
+++ create_node_tarball.sh
@@ -3,6 +3,6 @@
 version=$(rpm -q --specfile --qf='%{version}\n' nodejs.spec | head -n1)
 node_version=node-v${version}-rh
 
-git clone https://github.com/bucharest-gold/node.git -b v${version}-rh ${node_version}
+git clone https://github.com/nodeshift/node.git -b v${version}-rh ${node_version}
 tar -zcf ${node_version}.tar.gz ${node_version}
 rm -rf ${node_version}
